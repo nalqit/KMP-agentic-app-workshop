5d25f30a7459d7639f5f71bbfb00481b1ed4b623
nalqit <nalqit.06@gmail.com>
2025-08-19 20:00:53 +0200

    scrooling response

--- composeApp/src/commonMain/kotlin/ly/com/tahaben/agentic_workshop/kmp_agentic_app_workshop/App.kt
+++ composeApp/src/commonMain/kotlin/ly/com/tahaben/agentic_workshop/kmp_agentic_app_workshop/App.kt
@@ -3,10 +3,17 @@ package ly.com.tahaben.agentic_workshop.kmp_agentic_app_workshop
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -15,6 +22,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -30,11 +38,13 @@ fun App() {
         val scope = rememberCoroutineScope()
         var quetion by remember { mutableStateOf("") }
         var screentxt by remember { mutableStateOf("") }
+        val scrollState = rememberScrollState()
         Column(
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.primaryContainer)
                 .safeContentPadding()
-                .fillMaxSize(),
+                /*.fillMaxWidth(),*/
+            .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
         TextField(
@@ -47,7 +57,7 @@ fun App() {
             geminiApi.generateContent(prompt = quetion)
                 .collect { response ->
                     println("response =  ${response.text}")
-                    screentxt += "${response.text}"
+                    screentxt += "${response.text}\n"
 
                 }
 
@@ -57,9 +67,18 @@ fun App() {
             ) {
             Text("ask AI")
         }
-            Text(text = screentxt)
+            /*Text(text = screentxt)*/
 
-            }
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(scrollState)
+                .safeContentPadding()
+        ) {
+            Text(text = screentxt)
+        }
+        }
         var showContent by remember { mutableStateOf(false) }
 /*
         Column(
