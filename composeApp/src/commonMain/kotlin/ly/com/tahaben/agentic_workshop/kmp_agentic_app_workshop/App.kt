a9aa5462a18b82ee037d3b7c512fd6ff2b98c5d5
nalqit <nalqit.06@gmail.com>
2025-08-19 16:50:48 +0200

    commit 2

--- composeApp/src/commonMain/kotlin/ly/com/tahaben/agentic_workshop/kmp_agentic_app_workshop/App.kt
+++ composeApp/src/commonMain/kotlin/ly/com/tahaben/agentic_workshop/kmp_agentic_app_workshop/App.kt
@@ -18,13 +18,26 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import kmp_agentic_app_workshop.composeapp.generated.resources.Res
 import kmp_agentic_app_workshop.composeapp.generated.resources.compose_multiplatform
+import kotlinx.coroutines.launch
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
         val geminiApi = remember { geminiAI() }
+        val scope = rememberCoroutineScope()
+        Button(onClick = {
+            scope.launch {
+            geminiApi.generateContent(prompt = "how fast is a cat")
+                .collect { response ->
+                    println("response =  ${response.text}")
+                }
+            }
+        }) {
+            Text("ask AI")
+        }
         var showContent by remember { mutableStateOf(false) }
+/*
         Column(
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.primaryContainer)
@@ -35,6 +48,8 @@ fun App() {
             Button(onClick = { showContent = !showContent }) {
                 Text("Click!")
             }
+
+
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
                 Column(
@@ -45,6 +60,6 @@ fun App() {
                     Text("Compose: $greeting")
                 }
             }
-        }
+        }*/
     }
 }
