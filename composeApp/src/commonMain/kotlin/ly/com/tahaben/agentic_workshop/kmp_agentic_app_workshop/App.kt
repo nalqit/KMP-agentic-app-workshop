36b45ace242789ce9877ef33898b0718ce4878a2
nalqit <nalqit.06@gmail.com>
2025-08-21 10:45:01 +0200

    clear button

--- composeApp/src/commonMain/kotlin/ly/com/tahaben/agentic_workshop/kmp_agentic_app_workshop/App.kt
+++ composeApp/src/commonMain/kotlin/ly/com/tahaben/agentic_workshop/kmp_agentic_app_workshop/App.kt
@@ -1,33 +1,21 @@
 package ly.com.tahaben.agentic_workshop.kmp_agentic_app_workshop
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-import kmp_agentic_app_workshop.composeapp.generated.resources.Res
-import kmp_agentic_app_workshop.composeapp.generated.resources.compose_multiplatform
 import kotlinx.coroutines.launch
 
 @Composable
@@ -36,9 +24,11 @@ fun App() {
     MaterialTheme {
         val geminiApi = remember { geminiAI() }
         val scope = rememberCoroutineScope()
-        var quetion by remember { mutableStateOf("") }
+        var question by remember { mutableStateOf("") }
         var screentxt by remember { mutableStateOf("") }
         val scrollState = rememberScrollState()
+        val isloading = remember { mutableStateOf(false) }
+        val isclearable = remember { mutableStateOf(false) }
         Column(
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.primaryContainer)
@@ -48,25 +38,49 @@ fun App() {
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
         TextField(
-            value = quetion,
-            onValueChange = {newText -> quetion = newText},
+            value = question,
+            onValueChange = {newText -> question = newText},
             label = {Text("enter the quetion")}
         )
         Button(onClick = {
-            scope.launch {
-            geminiApi.generateContent(prompt = quetion)
-                .collect { response ->
-                    println("response =  ${response.text}")
-                    screentxt += "${response.text}\n"
+            isloading.value = true
+            isclearable.value = false
+            try{
+                scope.launch {
+                geminiApi.generateContent(prompt = question)
+                    .collect { response ->
+                        println("response =  ${response.text}")
+                        screentxt += "${response.text}"
+                        isloading.value = false
 
-                }
+
+                    }
+
 
             }
+            } catch (e:Exception){
+                e.printStackTrace()
+                isloading.value = false
+            }
 
-        }
-            ) {
+
+        },
+
+            enabled = !isloading.value
+        )
+            {
             Text("ask AI")
         }
+            Button(onClick = {
+
+                screentxt = ""
+                isclearable.value = true
+
+            },
+                enabled = !isclearable.value
+            ){
+                Text("Clear")
+            }
             /*Text(text = screentxt)*/
 
 
